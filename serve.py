cbb1441a4219f274fec8e3ed0d2f13db9157f2c3
Guillaume Filion <guillaume.filion@gmail.com>
2012-03-21 20:43:58 +0100

    Made a hard HTML server and synchronized with blog.

--- serve.py
+++ serve.py
@@ -1,29 +1,47 @@
 # -*- coding: utf-8 -*-
+import setup_django_version
 
 import os
+import re
 
 from google.appengine.ext import webapp
-from google.appengine.ext import db
-from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp.template import _swap_settings
 
-class Server(webapp.RequestHandler):
-   """Minimal server."""
-   def get(self):
-      template_vals = {}
-      template_path = os.path.join(
-          os.path.dirname(__file__),
-          'templates',
-          'base.html'
-      )
-      self.response.out.write(template.render(
-          template_path,
-          template_vals
-      ))
+import django.conf
+from django import template
+from django.template import loader
+
+
+TEMPLATE_DIRS = ['templates']
+
+
+def render_template(template_name, template_vals=None):
+   """Shameless port of the function of the same name
+   by Nick Johnson from Bloggart."""
+
+   old_settings = _swap_settings({'TEMPLATE_DIRS': TEMPLATE_DIRS})
+   try:
+      tpl = loader.get_template(template_name)
+      rendered = tpl.render(template.Context(template_vals))
+   finally:
+      _swap_settings(old_settings)
+   return rendered
+
+
+class HardHTMLServer(webapp.RequestHandler):
+   """Server for 'hard' HTML files. Provides only Django templating.
+   All hard HTML files have to be in the 'templates' directory."""
+
+   def get(self, path):
+      path = re.sub('/$', '', path)
+      if path == '':
+         path = 'index.html'
+      self.response.out.write(render_template(path))
 
 application = webapp.WSGIApplication([
-  ('/.*', Server),
-], debug=True)
+  ('/(.*)', HardHTMLServer),
+])
 
 
 def main():
