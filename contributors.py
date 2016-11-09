2a1e6421358687c7504a1add7f2aed297b095cc1
gui11aume <guillaume.filion@gmail.com>
2016-11-09 16:54:06 +0100

    Updated people page and CV.

--- contributors.py
+++ contributors.py
@@ -1,4 +1,5 @@
 ref = {
    'Guillaume Filion' : 'https://plus.google.com/102441667247494916086',
    'Heng-Chang Chen': 'https://plus.google.com/107393735048598816750',
+   'Eduard Valera Zorita': 'https://plus.google.com/107298350108699983384',
 }
