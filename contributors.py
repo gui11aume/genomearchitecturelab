05ce599084c1784bc6836463843eb1df048a51f2
gui11aume <guillaume.filion@gmail.com>
2016-12-09 17:05:36 +0100

    Added Ruggero as a contributor.

--- contributors.py
+++ contributors.py
@@ -2,4 +2,5 @@ ref = {
    'Guillaume Filion' : 'https://plus.google.com/102441667247494916086',
    'Heng-Chang Chen': 'https://plus.google.com/107393735048598816750',
    'Eduard Valera Zorita': 'https://plus.google.com/107298350108699983384',
+   'Ruggero Cortini': 'https://plus.google.com/112022866638787550013',
 }
